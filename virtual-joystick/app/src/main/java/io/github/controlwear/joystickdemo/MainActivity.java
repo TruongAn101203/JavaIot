e72eda6afc261a07cdabf67bbe3e2e6d121b797c
nkhoaa <ngokhoa0611@gmail.com>
2024-06-25 22:16:39 +0700

    code thu nhan tin hieu

--- virtual-joystick/app/src/main/java/io/github/controlwear/joystickdemo/MainActivity.java
+++ virtual-joystick/app/src/main/java/io/github/controlwear/joystickdemo/MainActivity.java
@@ -9,5 +9,78 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSeekSpeed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mViewSpeed.setText("Speed: " + String.valueOf(i));
+                maxSpeed = i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        mSeekServo.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mViewServo.setText("Servo: " + String.valueOf(i));
+                angleChange = i;
+                bright = i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ipAddress = ipAddressEditText.getText().toString();
+                String portStr = portEditText.getText().toString();
+
+                if (ipAddress.isEmpty() || portStr.isEmpty()) {
+                    connectButton.setText("NULL");
+                } else {
+                    serverPort = Integer.parseInt(portStr);
+                    try {
+                        serverAddress = InetAddress.getByName(ipAddress);
+                        socket = new DatagramSocket();
+                        backgroundThread.start();
+                        connectButton.setText("Connected");
+                    } catch (Exception e) {
+                        connectButton.setText("ERR");
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (socket != null) {
+            socket.close();
+        }
+    }
+
+    public int map(int x, int in_min, int in_max, int out_min, int out_max) {
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+
+
 }
