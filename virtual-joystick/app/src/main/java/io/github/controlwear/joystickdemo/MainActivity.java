861f2d641188bc0d5b7a57c79ec34bebd33d76e7
Rhyster3013 <dada2493013@gmail.com>
2024-06-25 22:18:34 +0700

    Anh xa sang UI

--- virtual-joystick/app/src/main/java/io/github/controlwear/joystickdemo/MainActivity.java
+++ virtual-joystick/app/src/main/java/io/github/controlwear/joystickdemo/MainActivity.java
@@ -1,9 +1,38 @@
 package io.github.controlwear.joystickdemo;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText ipAddressEditText, portEditText, dataEditText;
+    private Button connectButton;
+    private DatagramSocket socket;
+    private InetAddress serverAddress;
+    private int serverPort;
+
+    //
+    private TextView mTextViewAngleLeft;
+    private TextView mViewSpeed;
+    private TextView mViewServo;
+
+    private SeekBar mSeekSpeed;
+
+    private SeekBar mSeekServo;
+    private TextView mTextViewStrengthLeft;
+    TextView txtBtn3, txtBtn4;
+    private TextView mTextViewAngleRight;
+    private TextView mTextViewStrengthRight;
+//    private TextView mTextViewCoordinateRight;
+
+    int bleft = 100, bright = 50, maxSpeed = 50, angleChange = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
